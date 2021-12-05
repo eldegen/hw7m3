856cd4840a44b491f3ab846414f4b5a580c47e29
eldegen <eliyaz2761@gmail.com>
2021-12-05 14:57:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw7m3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hw7m3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.example.hw7m3.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(binding.navBottom, navController);
+    }
+}
